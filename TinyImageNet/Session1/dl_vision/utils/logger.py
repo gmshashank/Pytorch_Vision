f7b3acbd10c609489dc8589642c22f66ea3a9822
Shashank Mewada <gmshashank@gmail.com>
2021-04-16 17:41:37 +0100

    TinyImageNet Training files

---
+++ TinyImageNet/Session1/dl_vision/utils/logger.py
@@ -0,0 +1,25 @@
+import logging
+import sys
+
+log_level = logging.INFO
+
+
+def setup_logger(name):
+
+    logging.basicConfig(filename="logs/dl_vision.log", filemode="a")
+
+    logger = logging.getLogger(f"dl_vision - {name}")
+
+    # set the Log level
+    logger.setLevel(log_level)
+
+    logger_format = logging.Formatter(
+        "[ %(asctime)s -%(name)s ] %(levelname)s: %(message)s"
+    )
+
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(logger_format)
+    logger.addHandler(stream_handler)
+    logger.propagate = False
+
+    return logger
